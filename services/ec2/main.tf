411bae87db89f24520b819bb0ec5b59f6142f329
Steven Garlick <steven.garlick@wowinc.com>
2020-05-21 15:36:15 -0500

    added best practice ec2 module

---
+++ services/ec2/main.tf
@@ -0,0 +1,16 @@
+module "ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
+
+  name                   = var.ec2_name
+  instance_count         = var.ec2_instance_count
+
+  ami                    = var.ec2_ami
+  instance_type          = var.ec2_instance_type
+  key_name               = var.ec2_key_name
+  monitoring             = var.ec2_monitoring
+  vpc_security_group_ids = var.ec2_security_group_ids
+  subnet_id              = var.ec2_subnet_id
+
+  tags = var.default_tags
+}
---
+++ services/ec2/variables.tf
@@ -0,0 +1,43 @@
+variable "ec2_name" {
+    description = "Name of ec2 instance."
+    type = string
+}
+
+variable "ec2_instance_count" {
+    description = "Quantity of ec2 instances in the cluster."
+    type = number
+}
+
+variable "ec2_ami" {
+    description = "ID of the AMI that will be used for the EC2 instance."
+    type = string
+}
+
+variable "ec2_instance_type" {
+    description = "EC2 instance size (ie. t2.micro)."
+    type = string
+}
+
+variable "key_name" {
+    description = "name of ssh key generated."
+    type = string
+}
+
+variable "ec2_monitoring" {
+    description = "Boolean value of whether monitoring is turned on."
+    type = bool 
+}
+
+variable "ec2_vpc_security_group_ids" {
+    description = "A list of IDs of security groups to attach to the ec2."
+    type = list
+}
+
+variable "ec2_subnet_id" {
+    description = "The ID of the subnet where the ec2 will sit."
+    type = string
+}
+
+variable "default_tags" { 
+    type = "map" 
+}
