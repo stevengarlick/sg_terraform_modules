5a895624077c69dfced77173c074176301f064c8
Steven Garlick <steven.garlick@wowinc.com>
2020-05-21 22:46:06 -0500

    fixed security group variable name

--- services/ec2/main.tf
+++ services/ec2/main.tf
@@ -9,7 +9,7 @@ module "ec2_cluster" {
   instance_type          = var.ec2_instance_type
   key_name               = var.ec2_key_name
   monitoring             = var.ec2_monitoring
-  vpc_security_group_ids = var.ec2_security_group_ids
+  vpc_security_group_ids = var.ec2_vpc_security_group_ids
   subnet_id              = var.ec2_subnet_id
 
   tags = var.default_tags
