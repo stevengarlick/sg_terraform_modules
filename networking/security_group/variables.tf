8f717260c3367b608a3dc08e14d3f6ae345f2598
Steven Garlick <steven.garlick@wowinc.com>
2020-05-21 22:23:17 -0500

    changed cidr variable to list from map

--- networking/security_group/variables.tf
+++ networking/security_group/variables.tf
@@ -17,17 +17,7 @@ variable "sg_vpc_id" {
     type = string
 }
 
-variable "sg_ingress_cidr_blocks" {
-    description = "Ingress cidr ranges allowed in the security group."
-    type = list
-}
-
-variable "sg_ingress_rules" {
-    description = "Traffic and port type of the ingress traffic allowed in the security group."
-    type = string
-}
-
 variable "sg_ingress_with_cidr_blocks" {
     description = "fully customized ingress rule for security group."
-    type = map(string)
+    type = list
 }
